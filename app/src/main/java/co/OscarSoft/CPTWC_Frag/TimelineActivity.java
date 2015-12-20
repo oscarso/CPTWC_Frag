0a2b9844717cb026c865e020f8d101007a5463f5
Oscar So <oscarso@hotmail.com>
2015-12-20 14:11:29 -0800

    Added ic_profile but there is an issue. ActionBar is not shown at all.

--- app/src/main/java/co/OscarSoft/CPTWC_Frag/TimelineActivity.java
+++ app/src/main/java/co/OscarSoft/CPTWC_Frag/TimelineActivity.java
@@ -1,7 +1,6 @@
 package co.OscarSoft.CPTWC_Frag;
 
 import android.app.Activity;
-//import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -51,17 +50,21 @@ public class TimelineActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.action_menu, menu);
+        getMenuInflater().inflate(R.menu.menu_timeline, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        /*if (id == R.id.) {
+        if (id == -1) {
             return true;
-        }*/
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onProfileView(MenuItem mi) {
+        return;
     }
 
     public void onComposeAction(MenuItem mi) {
