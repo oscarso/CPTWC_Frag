6fca0a2f78cbf41e4f2d8d36099462c5b8c136dc
Oscar So <oscarso@hotmail.com>
2015-12-20 20:52:37 -0800

    Set screen_name in Profile ActionBar Title

--- app/src/main/java/co/OscarSoft/CPTWC_Frag/ProfileActivity.java
+++ app/src/main/java/co/OscarSoft/CPTWC_Frag/ProfileActivity.java
@@ -12,20 +12,36 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import co.OscarSoft.CPTWC_Frag.frags.UserTimelineFrag;
+import co.OscarSoft.CPTWC_Frag.models.User;
 
 
 public class ProfileActivity extends AppCompatActivity {
-
     public final static String SCREEN_NAME = "screen_name";
+    TwitterClient client;
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
         Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar_profile);
         setSupportActionBar(myToolbar);
+
+        client = TwitterApplication.getRestClient();
+        client.getUserInfo(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                user = User.fromJSON(response);
+                getSupportActionBar().setTitle(user.getScreenName());
+            }
+        });
 
         final String screenName = getIntent().getStringExtra(SCREEN_NAME);
         Log.d("DEBUG", "ProfileActivity: screenName=" + screenName);
